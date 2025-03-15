77626e5504a146cb16b2470eb80587ddb2ad9eeb
Sukjit Tangcharoen <67230096@dpu.ac.th>
2025-03-15 09:46:22 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>{/* Name */}</div>
+            <div>{"67230096 นางสาวสุขจิตร ตั้งเจริญ"}</div>
           </Container>
         </Navbar>
         <Container>
